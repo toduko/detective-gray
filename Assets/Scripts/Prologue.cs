e157a8662735a7232415bfe6b0f64925e8f9a4cb
toduko <teddyepich@gmail.com>
2021-03-03 22:50:19 +0200

    Added Prologue

---
+++ Assets/Scripts/Prologue.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Prologue : TextWriter
+{
+    private Text holder;
+
+    [TextArea]
+    [SerializeField]
+    private string input;
+
+    [SerializeField]
+    private float letterDelay;
+
+    [SerializeField]
+    private float lineDelay;
+
+    private void Awake()
+    {
+        holder = GetComponent<Text>();
+
+        StartCoroutine(WriteText(input, holder, letterDelay, lineDelay));
+    }
+
+}
---
+++ Assets/Scripts/TextWriter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TextWriter : MonoBehaviour
+{
+    protected IEnumerator WriteText(string input, Text holder, float letterDelay, float lineDelay)
+    {
+        string []lines = input.Split('\n');
+        for (int i = 0; i < lines.Length; ++i)
+        {
+            for (int j = 0; j < lines[i].Length; ++j)
+            {
+                holder.text += lines[i][j];
+                yield return new WaitForSeconds(letterDelay);
+            }
+            holder.text += '\n';
+            yield return new WaitForSeconds(lineDelay);
+        }
+    }
+}
